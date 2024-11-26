534f4d71b06a777638bb227a7476bd64dbb34c70
Phuong Nguyen <phuongnse@gmail.com>
2024-11-26 12:41:55 +0700

    Desktop sidebar and logo.

---
+++ components/sidebar.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React from "react";
+import { CoinsIcon, HomeIcon, LayersIcon, ShieldCheckIcon } from "lucide-react";
+import Logo from "@/components/logo";
+
+const routes = [
+  {
+    href: "",
+    label: "Home",
+    icon: HomeIcon,
+  },
+  {
+    href: "workflows",
+    label: "Workflows",
+    icon: LayersIcon,
+  },
+  {
+    href: "credentials",
+    label: "Credentials",
+    icon: ShieldCheckIcon,
+  },
+  {
+    href: "billing",
+    label: "Billing",
+    icon: CoinsIcon,
+  },
+];
+
+function DesktopSidebar() {
+  return (
+    <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
+      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+        <Logo />
+      </div>
+    </div>
+  );
+}
+
+export default DesktopSidebar;
